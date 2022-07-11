942e75b79799915527dfe16cfd4eee1904881487
Ahmed-Gul-gamer <79982810+Ahmed-Gul-gamer@users.noreply.github.com>
2022-07-11 16:19:43 +0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,8 @@
 CREATE DATABASE library;
+--created DB
+
+\c library
+--Connect to DB
 
 CREATE TABLE student(
     std_id SERIAL,
@@ -6,6 +10,7 @@ CREATE TABLE student(
     last_name VARCHAR(20) NOT NULL,
     PRIMARY KEY(std_id)
 );
+--Created a table named student
 
 CREATE TABLE books(
     book_id SERIAL,
@@ -17,4 +22,70 @@ CREATE TABLE books(
     PRIMARY KEY(book_id),
     FOREIGN KEY(borrowed_by) REFERENCES student(std_id)
 );
+--Created a table named books
+
+--Now inserting data into each table
+INSERT INTO student(first_name, last_name)
+VALUES ("Mike", "Tyson");
+
+INSERT INTO student(first_name, last_name)
+VALUES ("Jordan", "Peterson");
+
+INSERT INTO student(first_name, last_name)
+VALUES ("Ron", "Wesley");
+
+INSERT INTO student(first_name, last_name)
+VALUES ("Paulo", "Dybala");
+
+INSERT INTO student(first_name, last_name)
+VALUES ("John", "Martin");
+
+INSERT INTO student(first_name, last_name)
+VALUES ("Waseem", "Badami");
+
+INSERT INTO student(first_name, last_name)
+VALUES ("Kevin", "Hart");
+
+INSERT INTO student(first_name, last_name)
+VALUES ("Violet", "Evergarden");
+
+INSERT INTO student(first_name, last_name)
+VALUES ("Susan", "Ross");
+
+INSERT INTO student(first_name, last_name)
+VALUES ("Emma", "Watson");
+
+INSERT INTO books(book_name, author)
+VALUES ("The Great Gatsby", "F.Scott Fitzgerald");
+
+INSERT INTO books(book_name, author)
+VALUES ("12 Rules for Life", "Jordan Peterson");
+
+INSERT INTO books(book_name, author)
+VALUES ("Anna Karenina", "Tolstoy");
+
+INSERT INTO books(book_name, author)
+VALUES ("The Shining", "Stephen King");
+
+INSERT INTO books(book_name, author)
+VALUES ("The Little Prince", "Autoine de Saint");
+
+INSERT INTO books(book_name, author)
+VALUES ("Berserk", "Kentaro Miura");
+
+INSERT INTO books(book_name, author)
+VALUES ("Demon Slayer", "Koyoharu Gotouge");
+
+INSERT INTO books(book_name, author)
+VALUES ("Avatar", "James Cameron");
+
+INSERT INTO books(book_name, author)
+VALUES ("Captain Marvel", "Stan Lee");
+
+INSERT INTO books(book_name, author)
+VALUES ("The Darkknight", "Geoff Johns");
+
+
+
+
 
